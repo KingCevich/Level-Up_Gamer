2f820a3c43e824800bfd4257200e8605afb14342
ToniC-3PO <an.calderonm@duocuc.cl>
2025-09-08 02:19:55 -0300

    probando.2

---
+++ assets/js/validaciones.js
@@ -0,0 +1,105 @@
+function Validaciones() {
+            
+            
+            //Correo
+            var email = document.getElementById('exampleInputEmail2').value;
+            var reg = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
+            var valido = reg.test(email);
+            var duoc = email.endsWith("@duocuc.cl");
+
+            if(valido==true){
+                //alert("Correo valido");
+                
+                if(duoc){
+                alert("Cuenta Duoc detectada");
+                }
+
+            }else{
+                alert("Correo Invalido");
+                
+            }
+
+            
+
+            
+            //validacion contraseña
+            var contra = document.getElementById('InputPassword2').value;
+            var regpass = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&#]{8,15}/;
+            var validpass = regpass.test(contra);
+
+            if(validpass==true){
+                //alert("Contraseña segura");
+            }else{
+                alert("La Contraseña no cumple con los requisitos");  
+            }
+
+            var confipass = document.getElementById('InputPassword2.1').value;
+            var validConfipass = (confipass==contra);
+
+            if(validConfipass){
+                //alert("Confirmacion Valida");
+            }else{
+                alert("Confirmacion Invalida"); 
+            }
+
+            //Fecha nacimiento
+            var cumple = new Date(document.getElementById("FechNac").value);
+            var acty = new Date();
+            var edad = acty.getFullYear() - cumple.getFullYear();
+            var actm = acty.getMonth() - cumple.getMonth();
+            
+            if (actm < 0 || (actm === 0 && acty.getDate() < cumple.getDate())) {
+            edad--;
+            }
+
+            if(cumple.getFullYear()<1900){
+                alert("ponga una fecha valida");
+            }
+
+            if(edad >= 18){
+                //alert("eres mayor de edad");
+                edad = true;
+                
+                
+
+                if (acty.getDate() ==cumple.getDate() && acty.getDate() ==cumple.getDate() ){
+                    alert("Feliz Cumpleaños! ten x puntos");
+                    puntos=+1
+                    
+                }
+
+            }else{
+                alert("Tiene que ser mayor de edad");
+                
+            }
+
+            var si = valido && validpass && validConfipass && edad;
+
+            if(si){
+                alert("Cuenta Creada");
+                return true;
+
+
+            }else{
+                return false;
+            }
+
+            
+
+
+        } 
+        
+        let flag = true;
+        function ojito(){
+            //ocultar o mostrar ojo contraseña
+            if(flag){
+                document.getElementById("InputPassword2").type = "password";
+                document.getElementById("coniconMostrar").src ="assets/fotos/ocultar.png";
+                flag=false;
+
+            }else{
+                document.getElementById("InputPassword2").type = "text";
+                document.getElementById("coniconMostrar").src = "assets/fotos/mostrar.png";
+                flag=true;
+            } 
+        }  
