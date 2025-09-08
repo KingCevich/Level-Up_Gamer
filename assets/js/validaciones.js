fe6ebfbcb2307da1a15236e2a5a56937edb53942
DWNyZ <na.cifva7@gmail.com>
2025-09-08 16:56:32 +0000

    ojito dentro de validacion(mas practico)

--- assets/js/validaciones.js
+++ assets/js/validaciones.js
@@ -87,5 +87,19 @@ function Validaciones() {
 
 
         } 
-        
+    
+    let flag = true;
+    function ojito(){
+        //ocultar o mostrar ojo contraseña
+        if(flag){
+                document.getElementById("InputPassword2").type = "password";
+                document.getElementById("coniconMostrar").src ="assets/fotos/ocultar.png";
+                flag=false;
+
+        }else{
+                document.getElementById("InputPassword2").type = "text";
+                document.getElementById("coniconMostrar").src = "assets/fotos/mostrar.png";
+                flag=true;
+        } 
+    }
   
